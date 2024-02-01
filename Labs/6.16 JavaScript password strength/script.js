4042b22bc17d3c73764abbaa5cfe25a2b61477dd
Daniel Romero <danieljromero305@gmail.com>
2024-02-01 13:17:24 -0500

    Completed 6.16

--- Labs/6.16 JavaScript password strength/script.js
+++ Labs/6.16 JavaScript password strength/script.js
@@ -1,1 +1,31 @@
-// Your solution goes here 
+// Your solution goes here
+
+function isStrongPassword(password) {
+    let isLongEnough = false; // needs to be true
+    let hasPasswordInside = false; // needs to stay false
+    let containsUppercase = false; // needs to be true
+
+    if(password.length >= 8) {isLongEnough = true;}
+    if(password.includes("password")){hasPasswordInside = true;}
+    if(password !== password.toLowerCase()){containsUppercase = true;}
+
+    if (isLongEnough && !hasPasswordInside && containsUppercase) {
+        console.log("true")
+        return true;
+    }else{
+        return false;
+    }
+    /*} else if (!isLongEnough) {
+        console.log("false - Too short")
+        return false;
+    } else if (hasPasswordInside) {
+        console.log("false - Contains \"password\"");
+        return false;
+    } else if (!containsUppercase) {
+        console.log("false - No uppercase characters");
+        return false;
+    } else {
+        console.log(false);
+        return false;
+    }*/
+}
