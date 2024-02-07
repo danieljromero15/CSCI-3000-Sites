0b9b7a111204c3abdf55b027f4686a1e96499af5
Daniel Romero <danieljromero305@gmail.com>
2024-02-07 14:44:13 -0500

    Reset all Labs to their original state

--- Labs/6.15 JavaScript loops/script.js
+++ Labs/6.15 JavaScript loops/script.js
@@ -1,15 +1,5 @@
 function drawTriangle(triangleSize) {
 
    // Your solution goes here
-    let output = "";
-    for(let i = 1; i <= triangleSize; i++){
-        for(let j = 0; j < i; j++){
-            output = output.concat("*");
-        }
-        output = output.concat("\n");
-    }
-    output = output.slice(0, -1); // Literally only here because zybooks didn't like the newline at the very end, just removes that
-
-    console.log(output);
    
 }
--- Labs/6.20 JavaScript maps/script.js
+++ Labs/6.20 JavaScript maps/script.js
@@ -1,21 +1,3 @@
 function calcWordFrequencies() {
-   let list = prompt("Enter a list of words.");
-   list = list.split(" ");
-   let listMap = new Map();
-
-   list.forEach((listElement) => {
-       if(listMap.has(listElement)){
-           listMap.set(listElement, listMap.get(listElement) + 1);
-       }else{
-           listMap.set(listElement, 1);
-       }
-   });
-
-   for(let mapElement of listMap){
-       console.log(mapElement[0] + " " + mapElement[1]);
-   }
+   
 }
-
-/*window.onload = function (event){
-    calcWordFrequencies();
-}*/
