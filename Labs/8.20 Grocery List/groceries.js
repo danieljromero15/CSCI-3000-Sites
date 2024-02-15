7405acc56e6f8827a394ef1a3b94d23081e493b7
Daniel Romero <danieljromero305@gmail.com>
2024-02-15 16:14:56 -0500

    Completed 8.20

--- Labs/8.20 Grocery List/groceries.js
+++ Labs/8.20 Grocery List/groceries.js
@@ -1,74 +1,84 @@
 let groceryList = [];
 
 // Wait until DOM is loaded
-window.addEventListener("DOMContentLoaded", function() {
-   document.querySelector("#addBtn").addEventListener("click", addBtnClick);
-   document.querySelector("#clearBtn").addEventListener("click", clearBtnClick);
+window.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#addBtn").addEventListener("click", addBtnClick);
+    document.querySelector("#clearBtn").addEventListener("click", clearBtnClick);
 
-   // Load the grocery list from localStorage
-   groceryList = loadList();
-  
-   if (groceryList.length > 0) {
-      // Display list
-      for (let item of groceryList) {
-         showItem(item);
-      } 
-   }
-   else {
-      // No list to display
-      enableClearButton(false);
-   }     
+    // Load the grocery list from localStorage
+    groceryList = loadList();
+
+    if (groceryList.length > 0) {
+        // Display list
+        for (let item of groceryList) {
+            showItem(item);
+        }
+    } else {
+        // No list to display
+        enableClearButton(false);
+    }
 });
 
 // Enable or disable the Clear button
 function enableClearButton(enabled) {
-   document.querySelector("#clearBtn").disabled = !enabled;
+    document.querySelector("#clearBtn").disabled = !enabled;
 }
 
 // Show item at end of the ordered list
 function showItem(item) {
-   let list = document.querySelector("ol");
-   list.innerHTML += `<li>${item}</li>`;     
+    let list = document.querySelector("ol");
+    list.innerHTML += `<li>${item}</li>`;
 }
 
 // Add item to grocery list
 function addBtnClick() {
-   let itemTextInput = document.querySelector("#item");
-   let item = itemTextInput.value.trim();
-   if (item.length > 0) {
-      enableClearButton(true);
-      showItem(item);
-      groceryList.push(item);
+    let itemTextInput = document.querySelector("#item");
+    let item = itemTextInput.value.trim();
+    if (item.length > 0) {
+        enableClearButton(true);
+        showItem(item);
+        groceryList.push(item);
 
-      // Save groceryList to localStorage
-      saveList(groceryList);
-   }
+        // Save groceryList to localStorage
+        saveList(groceryList);
+    }
 
-   // Clear input
-   itemTextInput.value = '';
+    // Clear input
+    itemTextInput.value = '';
 }
 
 // Clear the list
 function clearBtnClick() {
-   enableClearButton(false);
-   groceryList = [];
-   let list = document.querySelector("ol");
-   list.innerHTML = "";
+    enableClearButton(false);
+    groceryList = [];
+    let list = document.querySelector("ol");
+    list.innerHTML = "";
 
-   // Remove the grocery list from localStorage
-   clearList();
+    // Remove the grocery list from localStorage
+    clearList();
 }
 
 // Complete the functions below
 
 function loadList() {
-   
+    try {
+        return localStorage.getItem("list").split(",");
+    } catch (TypeError) {
+        return [];
+    }
 }
 
 function saveList(groceryList) {
-  
+    let store = "";
+    //console.log(groceryList);
+    groceryList.forEach((item) => {
+        store += item + ",";
+    })
+    store = store.slice(0, -1);
+    //console.log("store:" + store);
+    localStorage.setItem("list", store);
 }
 
 function clearList() {
-   
+    localStorage.clear();
 }
